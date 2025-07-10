91d955cf82c0ca1c0592c332c0e7e4d05ce010cb
Yogabharathi3 <118899387+Yogabharathi3@users.noreply.github.com>
2025-07-10 14:57:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, '0');
+  seconds = String(seconds).padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('clock').innerText = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
